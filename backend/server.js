b9b1a628afacd4b7da710dfe0a6242ef2b64cf37
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2025-11-15 20:56:04 +0530

    stream changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 // middleware
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 
 app.use("/api/inngest", serve({client: inngest,functions}))
 
