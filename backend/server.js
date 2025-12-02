080efef5956ccb25233740a9ee8e0d0a853f46c7
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2025-12-02 16:29:06 +0530

    fix vercel

--- backend/server.js
+++ backend/server.js
@@ -38,13 +38,4 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
-  } catch (error) {
-    console.error("💥 Error starting the server", error);
-  }
-};
-
-startServer();
+export default app
