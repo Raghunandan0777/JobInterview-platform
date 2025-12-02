8418ba89f9bc006eff5f5694209d9e09c7e286d8
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2025-12-02 16:05:01 +0530

    change in cors

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,51 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 import cors from "cors";
-import { connectDB } from "./lib/db.js";
-import { clerkMiddleware } from "@clerk/express";
-import chatRoutes from "./routes/chatRoutes.js";
-import sessionRoute from "./routes/sessionRoute.js";
-import { inngest, functions } from "./lib/inngest.js";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
+
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
+const __dirname = path.resolve();
+
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://job-interview-platform-nu.vercel.app"
-    ],
+    origin: [ENV.CLIENT_URL],
+    "https://job-interview-platform-nu.vercel.app",
     credentials: true,
   })
 );
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use(express.json());
-app.use(clerkMiddleware());
-
-// API routes
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
-app.use("/api/sessions", sessionRoute);
+app.use("/api/sessions", sessionRoutes);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
 
 
-const start = async () => {
-  await connectDB();
-  app.listen(ENV.PORT, () =>
-    console.log("Server running on port", ENV.PORT)
-  );
+
+  
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
 };
 
-start();
+startServer();
